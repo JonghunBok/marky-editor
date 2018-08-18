5a2f03c15c6fbdd0e5920db7537c418327c7aec6
JonghunBok <sunlightjh194@gmail.com>
2018-08-18 14:42:27 +0900

    Write a prototype of this project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,110 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="header-container">
+      <app-header class="app-header"></app-header>
+      <select v-model="selectedStyle" id="style">
+        <option value="style1">style1</option>
+        <option value="style2">style2</option>
+        <option value="style3">style3</option>
+      </select>
+    </div>
+
+    <div class="containers">
+      <div class="container">
+        <textarea v-model="input" id="input" @input="update"></textarea>
+      </div>
+      <div class="container">
+        <div id="output" v-bind:class="selectedStyle" v-html="compiledMarkdown"></div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+
 
 export default {
-  name: 'app',
+  data() {
+    return {
+      input: '',
+      selectedStyle: 'style1'
+    }
+  },
+  computed: {
+    compiledMarkdown() {
+      return marked(this.input, {sanitzie: true})
+    }
+  },
+  methods: {
+    update: _.debounce(function (e) {
+      this.input = e.target.value
+    }, 300)
+
+  },
   components: {
-    HelloWorld
+    'app-header': Header
+  },
+  created() {
+    this.input = `
+# Hi1
+## This is a simple web app.
+    `;
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+.style1 { @import './styles/style1.scss'; }
+.style2 { @import './styles/style2.scss'; }
+
+.header-container {
+  display: flex;
+  flex-direction: row;
+  height: 60px;
 }
+
+.app-header {
+  flex-grow: 1;
+}
+
+#style {
+  flex-basis: 30%;
+  font-size: 1.2em;
+}
+
+.containers {
+  display: flex;
+  height: calc(100vh - 80px);
+}
+
+.container {
+  flex-basis: 50%;
+  padding: 5px;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  box-sizing: border-box;
+}
+
+#input {
+  margin-top: 5px;
+  padding: 0.6em;
+  border: 1px solid #ddd;
+  overflow: auto;
+  flex-grow: 1;
+  flex-shrink: 1;
+
+}
+
+#output {
+  margin-top: 5px;
+  border: 1px solid #ddd;
+  padding: 0.6em;
+  flex-grow: 1;
+  overflow: auto;
+}
+
+
+
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="header">
+    <h1>Marky Editor</h1>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+@import '../styles/variables.scss';
+#header {
+  
+}
+</style>
